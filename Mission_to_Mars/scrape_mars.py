f67b0c7b69c69df3c0972e6bc6ae17ea31fdf39b
gldn-god <78330405+gldn-god@users.noreply.github.com>
2021-06-07 12:28:07 -0600

    featured image and fact table functions built

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -18,11 +18,8 @@ def init_browser():
 # Scrape Most Recent News
 def news(browser):
     browser.visit('https://mars.nasa.gov/news/')
+    time.sleep(3)
 
-    # delay to allow time for page to load
-    time.sleep(5)
-
-    # create beautifulsoup object
     article_soup = BeautifulSoup(browser.html, 'html.parser')
     article_result = article_soup.find('article')
 
@@ -33,3 +30,37 @@ def news(browser):
     except:
         return None, None, None
     return news_date, news_title, news_p
+
+
+# Scrape Featured Image
+def featured(browser):
+    browser.visit('https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html')
+    time.sleep(3)
+    browser.links.find_by_partial_text('FULL IMAGE').click()
+    time.sleep(3)
+
+    image_soup = BeautifulSoup(browser.html, 'html.parser')
+
+    try:
+        image_result = image_soup.find(class_ = 'fancybox-image')['src']
+    except:
+        return None
+    featured_image_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{image_result}'
+    return featured_image_url
+
+
+# Scrape Facts Table
+def facts(browser):
+    browser.visit('https://space-facts.com/mars/')
+    time.sleep(3)
+
+    fact_soup = BeautifulSoup(browser.html, 'html.parser')
+    try:
+        fact_df = pd.read_html('https://space-facts.com/mars/')[0]
+    except:
+        return None
+    
+    fact_df.columns = ['Description', 'Mars']
+    fact_df.set_index('Description', inplace = True)
+    fact_html = fact_df.to_html(index = False, header = False)
+    return fact_html
