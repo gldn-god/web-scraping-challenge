5363ca39bd583f39058bd45aec42be4060eb5bd8
gldn-god <78330405+gldn-god@users.noreply.github.com>
2021-06-07 11:02:15 -0600

    flask app created with scrape and root routes

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,32 +1,39 @@
-# Import Dependencies
-from flask import Flask, render_template, redirect
-import pymongo
-# import scrape_mars.py script to get mars_data
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
 import scrape_mars
 
 app = Flask(__name__)
 
-mongo = pymongo(app, uri = "mongodb://localhost:27017/weather_app")
+# Use flask_pymongo to set up mongo connection
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars_app'
+mongo = PyMongo(app)
 
+# Or set inline
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
-# Root Route
 @app.route('/')
 def index():
-    mars_data = mongo.db.mars_data.find_one()
-    return render_template('index.html', mars_data=mars_data)
+    mars = mongo.db.mars.find_one()
+    return render_template(
+		'index.html', 
+		mars = mars
+	)
 
-# Scrape Route
+
 @app.route('/scrape')
 def scrape():
-	mars_data = mongo.db.mars_data
-	data = scrape_mars.scrape()
-	mars_data.update(
-		{},
-		data,
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update(
+		{}, 
+		mars_data, 
 		upsert = True
 	)
-	
-	return redirect("http://localhost:5000/", code = 302)
+    return redirect(
+		'/', 
+		code = 302
+	)
+
 
 if __name__ == '__main__':
-	app.run(debug = True)
+    app.run()
